880635433073b5cfe10e17fd70a2064047f035bb
Oisisushi <osushi.oisisushi@gmail.com>
2024-01-06 10:56:14 +0900

    コンストラクタの書式が間違ってたため修正

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -7,22 +7,48 @@ public class BaseBallTeam {
     private int draw;//    引き分け数
 
     //念のため作る引数なしコンストラクタ
-    public baseball() {
+    public BaseBallTeam() {
     }
-    //引数4つ、球団生成用コンストラクタ
-    public baseball(String name, int win, int lose, int draw) {
+    //引数4つ、BaseBallTeam生成用コンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int draw) {
         this.name = name;
         this.win = win;
         this.lose = lose;
         this.draw = draw;
     }
-
+    //doubleで返す勝率計算メソッド
     public double getRate() {
 
     }
-
+    //球団の成績表示メソッド
     public String report() {
 
+    }
+
+    //getterとsetter
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWin() {
+        return win;
+    }
+    public void setWin(int win) {
+        this.win = win;
+    }
+    public int getLose() {
+        return lose;
+    }
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+    public int getDraw() {
+        return draw;
+    }
+    public void setDraw(int draw) {
+        this.draw = draw;
     }
 
 }
